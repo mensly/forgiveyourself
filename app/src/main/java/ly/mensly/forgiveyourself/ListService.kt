c5b1f97c538c0b2230f1e0e0429584e311fa264f
Michael Ensly <michael@domestic.cat>
2021-01-30 10:24:47 +1100

    persist list

--- app/src/main/java/ly/mensly/forgiveyourself/ListService.kt
+++ app/src/main/java/ly/mensly/forgiveyourself/ListService.kt
@@ -1,13 +1,20 @@
 package ly.mensly.forgiveyourself
 
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import androidx.core.content.edit
+
 class ListService private constructor() {
     companion object {
         val instance = ListService()
+        private const val KEY_MISTAKES = "mistakes"
     }
 
     val list: List<String> get() = mistakes
-    private val mistakes = mutableListOf<String>()
+    private val mistakes by lazy { loadMistakes().toMutableList() }
     private val listChanged = mutableListOf<(List<String>) -> Unit>()
+    @Suppress("DEPRECATION")
+    private val sharedPreferences by lazy { PreferenceManager.getDefaultSharedPreferences(App.instance) }
 
     fun addListChangedListener(listener: (List<String>)->Unit) {
         listChanged += listener
@@ -18,5 +25,14 @@ class ListService private constructor() {
     fun addItem(mistake: String) {
         mistakes.add(mistake)
         listChanged.forEach { it(mistakes) }
+        saveMistakes()
     }
+
+    private fun saveMistakes() {
+        sharedPreferences.edit {
+            putStringSet(KEY_MISTAKES, mistakes.toSet())
+        }
+    }
+
+    private fun loadMistakes() = sharedPreferences.getStringSet(KEY_MISTAKES, emptySet())!!
 }
